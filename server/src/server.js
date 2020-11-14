3815b487984f428b2388e6fd9665d3dad5c83fab
Camille Hagenbourger <camille.hagenbourger@protonmail.ch>
2020-11-14 14:11:40 +0100

    ready to serve https

--- server/src/server.js
+++ server/src/server.js
@@ -1,7 +1,8 @@
 import express from "express";
-import cors from "cors";
 import path from "path";
 import session from "express-session";
+import fs from "fs";
+import https from "https";
 import connectMongoDBSession from "connect-mongodb-session";
 import passport from "passport";
 import bodyParser from "body-parser";
@@ -12,28 +13,31 @@ import apiRoutes from "./routes/api.routes";
 import authRoutes from "./routes/auth.routes";
 import sessionMiddlewares from "./auth/session/session.middlewares";
 
+// redirecting http requests
 const httpApp = express();
 
-// redirecting http requests
-// httpApp.use((req, res, next) => {
-//   var host = req.get('Host');
-//   if (host === 'http://manywords.org') {
-//     return res.redirect(301, 'https://manywords.org/' + req.originalUrl);
-//   }
-//   return next();
-// });
+httpApp.get("/*", (req, res) => {
+  console.info("Redirect user to https")
+  return res.redirect(301, 'https://manywords.org/');
+});
+httpApp.listen(80, () => {
+  console.info(`Http server ready to redirect from port 80`);
+});
 
-// const sslOptions = {
-//   key: fs.readFileSync('certificates/privkey.pem','utf8'),
-//   cert: fs.readFileSync('certificates/cert.pem','utf8'),
-//   ca: fs.readFileSync('certificates/chain.pem','utf8'),
-// };
+// https server
+// TODO: path as .env variables
+const sslOptions = {
+  key: fs.readFileSync('/server/letsencrypt/live/manywords.org-0002/privkey.pem','utf8'),
+  cert: fs.readFileSync('/server/letsencrypt/live/manywords.org-0002/cert.pem','utf8'),
+  ca: fs.readFileSync('/server/letsencrypt/live/manywords.org-002/chain.pem','utf8'),
+};
 
-// const app = https.createServer(sslOptions, app);
+const app = express();
+const httpsApp = https.createServer(sslOptions, app);
 
 /*  -------------   login and session middlewares    -----------*/
 const MongoDBStore = connectMongoDBSession(session);
-app.set("trust proxy", 1);
+//app.set("trust proxy", 1); needed in development only?
 app.use(
   session({
     secret: secrets.SESSION_SECRET,
@@ -70,7 +74,7 @@ db.on("error", console.error.bind(console, "MongoDB connection error:"));
 db.once("open", () => {
   console.info("Connected to database");
   // configuring the listening port
-  const listener = app.listen(3001, () => {
+  const listener = httpsApp.listen(3001, () => {
     console.info(`Many-words is listening on port ${listener.address().port}`);
   });
 });
